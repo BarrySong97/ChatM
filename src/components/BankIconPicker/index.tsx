4f14f46b3e69f31c3c57035c041b168275633080
Barry Song <524000659@qq.com>
2024-09-04 09:29:08 +0800

    feat(bankpicker):实现一个简单的银行iconpicker

---
+++ src/components/BankIconPicker/index.tsx
@@ -0,0 +1,66 @@
+import { Button, Card, CardHeader, Divider, Input } from "@nextui-org/react";
+import React, { FC, useState } from "react";
+import bankcode from "./bank.json";
+
+type BankCode = Array<{
+  name: string;
+  logo: string;
+}>;
+
+const typedBankcode: BankCode = bankcode;
+export interface BankIconPickerProps {
+  value?: string;
+  onChange?: (value: string) => void;
+}
+const BankIconPicker: FC<BankIconPickerProps> = ({ value, onChange }) => {
+  const [focusBank, setFocusBank] = useState<{
+    logo: string;
+    name: string;
+  }>({
+    logo: "",
+    name: "",
+  });
+  return (
+    <Card className="max-w-[300px]  py-2 px-2">
+      <CardHeader className="px-0">
+        <Input size="sm" radius="sm" placeholder="请输入银行名称" />
+      </CardHeader>
+      <div className=" h-[300px] overflow-y-auto scrollbar ">
+        <div className="grid grid-cols-6 gap-2">
+          {typedBankcode.map((item) => {
+            return (
+              <div key={item.name} className="cursor-pointer">
+                <Button
+                  variant="light"
+                  isIconOnly
+                  size="sm"
+                  radius="sm"
+                  onMouseEnter={() => setFocusBank(item)}
+                >
+                  <img className="h-5 w-5" src={item.logo} alt={item.name} />
+                </Button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <Divider className="mt-2 mb-2" />
+      <div className="flex items-center gap-2">
+        {focusBank.logo ? (
+          <img
+            className="h-10 w-10"
+            src={focusBank.logo}
+            alt={focusBank.name}
+          />
+        ) : (
+          <div className="h-10 w-10 bg-gray-200 rounded-sm"></div>
+        )}
+        <div className="text-base font-semibold">
+          {focusBank.name ? focusBank.name : "请选择一个银行"}
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default BankIconPicker;
--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,30 +1,10 @@
+import BankIconPicker from "@/components/BankIconPicker";
 import ExpandTreeMenu, { TreeNode } from "@/components/ExpandTreeMenu";
 import React, { FC, useState } from "react";
 export interface SettingsProps {}
 const Settings: FC<SettingsProps> = () => {
-  const treeData: TreeNode[] = [
-    {
-      id: "1",
-      label: "Getting Started",
-      children: [
-        { id: "1-1", label: "AFFINE - not just a note-taking ap" },
-        { id: "1-2", label: "Templates Galleries" },
-      ],
-    },
-    { id: "2", label: "素材库" },
-    { id: "3", label: "未命名" },
-    { id: "4", label: "22" },
-  ];
-  const [selectedKey, setSelectedKey] = useState<string>();
-
   // 在你的应用中使用
-  return (
-    <ExpandTreeMenu
-      data={treeData}
-      selectedKey={selectedKey}
-      onSelectionChange={setSelectedKey}
-    />
-  );
+  return <BankIconPicker />;
 };
 
 export default Settings;
